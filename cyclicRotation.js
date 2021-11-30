9077ef1697f5adf48c6ceb701dbb5bb5a3907919
MJ <mj@anniecannons.com>
2021-11-30 10:55:57 -0800

    cyclicRotation exercise

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -2,9 +2,11 @@
 // * Created by yoneta on 3/23/16.
 // */
 
-//A zero-indexed array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is also moved to the first place.
+//A zero-indexed array A consisting of N integers is given. Rotation of the array means that each element 
+//is shifted right by one index, and the last element of the array is also moved to the first place.
 //
-//    For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]. The goal is to rotate array A K times; that is, each element of A will be shifted to the right by K indexes.
+//    For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]. The goal is to rotate array A K times; 
+//    that is, each element of A will be shifted to the right by K indexes.
 //
 //    Write a function:
 //
@@ -14,6 +16,13 @@
 //
 //    For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].\
 
-function cyclicRotation(arr, rotation){
-    //Your code here...
+function cyclicRotation(arr, rotation) {
+    for (let i = 0; i < rotation; i++) {
+        arr.unshift(arr.pop());
+    }
+    console.log(arr);
 }
+
+cyclicRotation([3, 4, 5, 6, 7], 2);
+
+//BONUS: Add if/else conditional statement for negative rotation numbers
